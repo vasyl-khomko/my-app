d77a1330745a6249293550b9929ded99d0c36f59
vasyl-khomko <homko.wasyl@gmail.com>
2021-06-09 17:27:11 +0300

    Add transactions and stored procedures

---
+++ src/main/java/com/mycompany/app/InsertStoredProcedure.java
@@ -0,0 +1,20 @@
+package com.mycompany.app;
+
+import java.sql.*;
+import java.time.LocalDate;
+
+public class InsertStoredProcedure {
+
+    public static void main(String[] args) throws SQLException {
+        Connection con = ConnectionUtils.getConnection();
+        PreparedStatement statement = con.prepareCall("{call insertData(?, ?, ?, ?, ?, ?)}");
+
+        statement.setString(1, "SP-11");
+        statement.setString(2, "John Stone");
+        statement.setString(3, "John Doe");
+        statement.setDate(4, Date.valueOf(LocalDate.of(1994, 12, 30)));
+        statement.setInt(5, 95);
+        statement.setDate(6, Date.valueOf(LocalDate.of(2021, 1, 15)));
+        statement.executeQuery();
+    }
+}
